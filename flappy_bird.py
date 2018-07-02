67abc8bab2bf220b27b5994401040f884c5a3d1a
Ryan Tomlinson <rltomlinson@outlook.com>
2018-07-02 09:43:00 -0500

    initial commit

---
+++ flappy_bird.py
@@ -0,0 +1,16 @@
+# I found this rubric on reddit...Thought Id give it a shot
+# Make an object appear on the screen.
+# Make it something other than a rectangle (one student chose my face).
+# Make it move SOMEHOW based on keyboard input. It doesn't have to be Flappy Bird movement.
+# Make it bounce with gravity after given keyboard input. This is really the only thing that is math-y in the project.
+# Prevent it from going off the screen.
+# Make a pipe appear.
+# Make the pipe move left across the screen.
+# Make a bottom pipe appear below a top pipe and move together across the screen.
+# Generate a new top/bottom pipe after a certain amount of time (or once the last pipes reach a certain point).
+# Freeze everything if the bird collides with either pipe.
+# Create a score that increases for every pipe the bird passes.
+# Allow the player to reset the game after death.
+# Hold onto a high score.
+# Create frame animations for the bird.
+
